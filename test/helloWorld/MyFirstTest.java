89fbe04d3a5dc6a68a1394eb7d11513605c02d40
sadish-vz <sadishkumar.x.neelamegam@verizon.com>
2015-09-29 11:09:30 -0700

    Changed for Test Case

--- test/helloWorld/MyFirstTest.java
+++ test/helloWorld/MyFirstTest.java
@@ -1,17 +1,12 @@
-package Test;
+package helloWorld;
 
 import static org.junit.Assert.*;
 
 import itsfor.testing.ForTesting;
-
-import org.junit.Before;
 import org.junit.Test;
 
 public class MyFirstTest {
 
-	@Before
-	public void setUp() throws Exception {
-	}
 
 	@Test
 	public void test() {
